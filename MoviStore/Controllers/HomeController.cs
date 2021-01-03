ce4d0b09b05232058d16ed7b906cefdf04f0ccdf
Hoàn Nguyễn <softteam2021@gmail.com>
2021-01-03 11:40:52 +0700

    init project

---
+++ MoviStore/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MoviStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie
+                {
+                    Id = 1,
+                    Director = "Peter Cameron",
+                    Name = "Star War",
+                    Year = 2020,
+                    Ownner = "Wanderos"
+                },
+                 new Movie
+                {
+                    Id = 2,
+                    Director = "Hoan",
+                    Name = "Return of Kings",
+                    Year = 2019,
+                    Ownner = "Columbia Picture"
+                },
+                  new Movie
+                {
+                    Id = 3,
+                    Director = "Tien",
+                    Name = "Fairy",
+                    Year = 2020,
+                    Ownner = "Sony Picture"
+                },
+                   new Movie
+                {
+                    Id = 4,
+                    Director = "Cameron",
+                    Name = "Superman",
+                    Year = 2020,
+                    Ownner = "DC"
+                },
+                    new Movie
+                {
+                    Id = 5,
+                    Director = "ABC",
+                    Name = "Avenger",
+                    Year = 2020,
+                    Ownner = "Marvel Studio"
+                },
+                new Movie
+                {
+                    Id = 6,
+                    Director = "Peter",
+                    Name = "Doremon",
+                    Year = 2020,
+                    Ownner = "Wanderos"
+                }
+            };
+            return View(movies);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MoviStore/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    if (location.href.indexOf("Movie") !== -1) {
+        $(".movie-nav").addClass("active");
+    }
+    else if (location.href.indexOf("Home/Privacy") !== -1) {
+        $(".privacy-nav").addClass("active");
+    } else {
+        $(".home-nav").addClass("active");
+    }
+});
